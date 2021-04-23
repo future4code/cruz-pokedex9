d1895d3915b7eb0a9dfb687cd3c3e332337861b6
Fagner Zulin <fagnerzulin11@gmail.com>
2021-04-23 13:36:28 -0300

    Done styled-container

---
+++ pokedex/src/components/Container/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  width: 80vw;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px;
+  row-gap: 20px;
+  margin: 0 auto;
+`;
--- pokedex/src/pages/PokedexPage.js
+++ pokedex/src/pages/PokedexPage.js
@@ -1,28 +1,30 @@
 import React, { useContext } from "react";
 import PokemoCard from "../components/Card/PokemoCard";
+import Container from "../components/Container/Container";
 import Header from "../components/Header/Header";
 import { GlobalStateContext } from "../global/GlobalStateContext";
 
 export default function PokedexPage() {
-  const { states, setters, changes } = useContext(GlobalStateContext);
+  const { states,  changes } = useContext(GlobalStateContext);
 
   return (
     <div>
       <Header title="Pokédex" hasSecondButton closeButton />
-      {states.pokedex.length > 0 &&
-        states.pokedex.map((pokemon) => {
-          return (
-            <PokemoCard
-              key={pokemon.id}
-              id={pokemon.id}
-              name={pokemon.name}
-              photo={pokemon.details.photos.front}
-              textButton='Remover da pokédex'
-              onClick={changes.removeToPokedex}
-            />
-          );
-        })}
-      <h1>PokedexPage</h1>
+      <Container>
+        {states.pokedex.length > 0 &&
+          states.pokedex.map((pokemon) => {
+            return (
+              <PokemoCard
+                key={pokemon.id}
+                id={pokemon.id}
+                name={pokemon.name}
+                photo={pokemon.details.photos.front}
+                textButton="Remover da pokédex"
+                onClick={changes.removeToPokedex}
+              />
+            );
+          })}
+      </Container>
     </div>
   );
 }
